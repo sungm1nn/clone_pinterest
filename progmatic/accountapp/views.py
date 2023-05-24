ab464b81c26a87a781ac36f17571baa2007f4ab0
sungm1nn <jjsm4690@naver.com>
2023-05-24 20:44:08 +0900

    get post db test

--- progmatic/accountapp/views.py
+++ progmatic/accountapp/views.py
@@ -1,7 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from accountapp.models import HelloWorld
+
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    
+    if request.method == "POST":
+        
+        temp = request.POST.get('hello_world_input')
+        
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+        
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
