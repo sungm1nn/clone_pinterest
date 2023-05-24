9905cad157d0cc65dd12723982cd36662db5b06a
sungm1nn <jjsm4690@naver.com>
2023-05-24 20:49:31 +0900

    dbtest

--- progmatic/accountapp/views.py
+++ progmatic/accountapp/views.py
@@ -15,6 +15,9 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
         
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        hello_world_list = HelloWorld.objects.all()
+        
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
