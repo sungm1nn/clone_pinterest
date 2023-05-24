22171924b3fc0ad3e0ea9183902a1763dd7ede7d
sungm1nn <jjsm4690@naver.com>
2023-05-24 13:27:04 +0900

    gitignore

--- progmatic/progmatic/settings.py
+++ progmatic/progmatic/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from .my_settings import MY_SECRET
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ea48np&&d$yi*1rh3t0s@!g8%l2i$)7llx6y0g4yc5u#%kbvux'
+SECRET_KEY = MY_SECRET['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
