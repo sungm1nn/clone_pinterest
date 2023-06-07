571060718eb67ab10fc4397ac787b48c413a57d8
sungm1nn <sungmin@DESKTOP-UN2TGEL>
2023-06-07 14:22:16 +0900

    edit

--- progmatic/progmatic/settings/deploy.py
+++ progmatic/progmatic/settings/deploy.py
@@ -1,31 +1,39 @@
 from .base import *
 
+
 def read_secret(secret_name):
-    file = open('/run/secret/'+secret_name)
+    file = open('/run/secrets/' + secret_name)
     secret = file.read()
-    secret.strip()
+    secret = secret.rstrip().lstrip()
     file.close()
     return secret
+
 
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
 
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# reading .env file
+environ.Env.read_env(
+    env_file= os.path.join(BASE_DIR, '.env')
+)
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+
 # Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
